c9f8f278b933168208cfff651ac403d6188b365d
RizkiNurmahmudi <rizkinurmahmudi@gmail.com>
2025-06-11 18:32:17 +0700

    Delete Catur VERSI 1.0 directory

--- Catur VERSI 1.0/io/color.c
+++
@@ -1,9 +0,0 @@
-#include "color.h"
-
-void setRgbColor(int r, int g, int b) {
-    printf("\033[38;2;%d;%d;%dm", r, g, b);
-}
-
-void setRgbBackground(int r, int g, int b) {
-    printf("\033[48;2;%d;%d;%dm", r, g, b);
-}
